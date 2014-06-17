7c2e084cf066b52d86975b1c8b55c6c3d83b3ba2
brad <bwalker8038@gmail.com>
2014-06-17 10:35:26 -0400

    fixes for bwalker8038/generator-bakery #5

--- app/templates/_config.js
+++ app/templates/_config.js
@@ -1,17 +1,32 @@
 var config = {
   host: 'localhost',
-  port: process.env.PORT || 3030,
+  port: parseInt(+process.env.PORT, 10) || 3030,
 
   app: {
     cache: {
-      engine: 'memory'
+      engine: 'catbox-memory'
     },
+
+    // Currently set to debug all errors, change if needed
+    debug: ['errors'],
 
     views: {
       path: __dirname + '/views',
       engines: { jade: 'jade' },
       compileOptions: { pretty: true }
     }
+  },
+
+  dev: {
+    /* Dev specific config options here
+     * i.e. enviroment specific credentials, database connections
+     */
+  },
+
+  prod: {
+    /* Prod specific config options here
+     * i.e. enviroment specific credentials, database connections
+     */
   }
 };
 
--- app/templates/_server.js
+++ app/templates/_server.js
@@ -4,22 +4,30 @@
 var hapi = require('hapi');
 var config = require('./config');
 
+// ENV Config Options
+var envConf;
+if ( process.env.NODE_ENV === 'dev' ) {
+  envConf = config.dev;
+} else {
+  envConf = config.prod;
+}
 
-exports.start = function() {
-  var app = new hapi.Server('localhost', config.port, config.app);
+exports.start = function () {
+  var app = new hapi.Server(config.host, config.port, config.app);
 
-  // Routing Table
+  // Routes
   var routes = [
     'static',
     'home'
   ];
 
-  routes.map(function(name) {
+  routes.map(function( name) {
     var controller = require('./controllers/' + name);
     controller.setup(app);
   });
 
-  app.start(function() {
+  app.start(function () {
+    console.log('Server running in ' + process.env.NODE_ENV + ' environment');
     console.log('Hapi server listening on port ' + config.port);
   });
 };
